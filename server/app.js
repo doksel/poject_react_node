03200ed654728a7310a8a3fd9ed69d4166e42e03
doksel <doksel7@gmail.com>
2019-01-24 23:11:20 +0200

    add webpack

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const Users = require('./models/users');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/javascrips',express.static(path.join(__dirname, 'node_modules', 'jquery','dist')));
 
 let data = [
     {
