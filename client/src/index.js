be2a3cecb3f44a66d439657128a791df22d7ba58
doksel <doksel7@gmail.com>
2019-03-24 09:43:33 +0200

    include redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './redux/store';
-// import store from './redux/redux_store';
-import {BrowserRouter} from "react-router-dom";
-
 import * as serviceWorker from './serviceWorker';
 
-let rerender = () => {
-    ReactDOM.render(
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extention';
+import rootReducer from './.'
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+
+ReactDOM.render(
+    <Provider store={store}>
         <BrowserRouter>
-            <App data={store.getState()} getAllPost={store.getAllPost.bind(store)}/>
-        </BrowserRouter>, document.getElementById('root'));
-}
-rerender(store.getState());
-store.subscribe(store._rerender);
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
+    
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
---
+++ client/src/reducers/rrotReducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import newsReducer from './news_reducer';
+import usersReducer from './users_reducer';
+
