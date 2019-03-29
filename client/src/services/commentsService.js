42477964e80697b79463b0c0522057f609f82b9a
Andriy Fedorchenko <doksel7@gmail.com>
2019-03-29 18:01:46 +0200

    try add creating comment

--- client/src/services/commentsService.js
+++ client/src/services/commentsService.js
@@ -6,8 +6,8 @@ export const getAllCommentsDb = () => {
 export const getCommentDb = (id) => {
     return axios.get(`http://localhost:3001/api/commentsPosts/${id}`)
 }
-export const createCommentDb = (post) => {
-    return axios.post("http://localhost:3001/api/commentsPosts",post)
+export const createCommentDb = (comment) => {
+    return axios.post("http://localhost:3001/api/commentsPosts",comment)
 }
 export const deleteCommentDb = (id) => {
     return axios.delete(`http://localhost:3001/api/commentsPosts/${id}`)
