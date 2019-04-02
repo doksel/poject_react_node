30b80ef858f430a44f57477cffdb9cb10aff8f70
Andriy Fedorchenko <doksel7@gmail.com>
2019-04-02 17:07:19 +0300

    added forms login and registration in client

--- client/src/components/content/login/login.jsx
+++ client/src/components/content/login/login.jsx
@@ -4,10 +4,46 @@ import './login.css';
 let Login = (props) => {
     return(
     <div className="login">
-        <form id="login_form">
+        <form className="form_register" method="POST">
             <div className="form_inner">
-                <textarea name="task" id="addTask" cols="30" rows="10"/>
-                <button className="btn_form js-addTask">add task</button>
+                <h2 className="form-register-heading">Please register</h2>
+                <div className="form-control">
+                    <label for="registerEmail" className="label">Email address</label>
+                    <input type="email" id="registerEmail" name="email" className="input" placeholder="Email address" required autofocus></input>
+                </div>
+                <div className="form-control">
+                    <label for="registerUsername" className="label">Username</label>
+                    <input type="text" id="registerUsername" name="login" className="input" placeholder="Username" required></input>
+                </div>
+                <div className="form-control">
+                    <label for="registerPassword" className="label">Password</label>
+                    <input type="password" id="registerPassword" name="password" className="input" placeholder="Password" required></input>
+                </div>
+                <div className="form-control">
+                    <label for="registerConfirmPassword" className="sr-only">Confirm Password</label>
+                    <input type="password" id="registerConfirmPassword" name="passwordConfirm" className="input" placeholder="Confirm Password" required></input>
+                </div>
+                <span className="error"></span>
+                <button className="btn_form btn-register" type="submit">Register</button>
+            </div>
+        </form>
+        
+        <form className="form_login" method="POST">
+            <div className="form_inner">
+                <h2 className="form-signin-heading">Please login</h2>
+                <div className="form-control">
+                    <label for="loginUsername" class="label">Username</label>
+                    <input type="text" id="loginUsername" name="login" className="input" placeholder="Username" required></input>
+                </div>
+                <div className="form-control">
+                    <label for="loginPassword" class="label">Password</label>
+                    <input type="password" id="loginPassword" name="password" className="input" placeholder="Password" required></input>
+                </div>
+                <span className="error"></span>
+                <div className="buttons_row">
+                    <button className="btn_form btn-login" type="submit">Login</button>
+                    <button className="btn_form change-register" type="submit">Register</button>
+                </div>
             </div>
         </form>
     </div>
